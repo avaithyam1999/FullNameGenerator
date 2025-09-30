934d09b63afbe598190f273c8205dfa6bad1b9c3
AJ V <avaithyam@yahoo.com>
2025-09-30 14:59:37 -0400

    updated commit

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -8,26 +8,30 @@ public class FullNameApplication {
 
         System.out.println("Enter your First Name: ");
         String firstName = scanner.nextLine();
+        firstName = firstName.trim();
 
         System.out.println("Enter your Middle Name if you have one: ");
         String middleName = scanner.nextLine();
+        middleName = middleName.trim();
 
         System.out.println("Enter your Last Name: ");
         String lastName = scanner.nextLine();
+        lastName = lastName.trim();
 
         System.out.println("Enter your suffix if you have one: ");
         String suffix = scanner.nextLine();
+        suffix = suffix.trim();
 
         System.out.println("Your full name is " + joinedName(firstName, middleName, lastName, suffix));
     }
     public static String joinedName(String firstName, String middleName, String lastName, String suffix) {
-        if (suffix.equals("") && middleName.equals("")) {
+        if (suffix.isEmpty() && middleName.isEmpty()) {
             return firstName + " " + lastName;
         }
-        else if (suffix.equals("")) {
+        else if (suffix.isEmpty()) {
             return firstName + " " + middleName + " " + lastName;
         }
-        else if (middleName.equals("")) {
+        else if (middleName.isEmpty()) {
             return firstName + " " + lastName + ", " + suffix;
         }
         else {
